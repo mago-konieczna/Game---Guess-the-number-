9532eca00b30fc4bc093e47d48fc6bec1145b499
Malgorzata Konieczna <gola434@wp.pl>
2022-10-10 12:59:29 +0200

    change2

--- Game - Guess the number!/Program.cs
+++ Game - Guess the number!/Program.cs
@@ -15,5 +15,57 @@
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("Guess the number from 1 to 10!\n ");
         Console.ResetColor();
+
+        do
+        {
+            do
+            {
+                Console.Write("Enter a number: ");
+                valid = int.TryParse(Console.ReadLine(), out user);
+                if (!valid)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Enter the correct number!");
+                    Console.ResetColor();
+                }
+
+            } while (!valid);
+
+            if (user < 1 || user > 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Enter a number from 1 to 10!");
+                Console.ResetColor();
+                continue;
+
+            }
+
+            if (array[user - 1])
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The same number was given!");
+                Console.ResetColor();
+                continue;
+            }
+            else
+            {
+                array[user - 1] = true;
+            }
+
+            if (random < user)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The drawn number is smaller than yours!");
+                Console.ResetColor();
+            }
+            else if (random > user)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The drawn number is greater than yours!");
+                Console.ResetColor();
+            }
+            i++;
+
+        } while (user != random);
     }
 }
