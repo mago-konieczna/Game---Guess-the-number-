f16432c659e516e0e40e1eb765ebfcadc4b922ca
Malgorzata Konieczna <gola434@wp.pl>
2022-10-10 12:58:24 +0200

    change1

--- Game - Guess the number!/Program.cs
+++ Game - Guess the number!/Program.cs
@@ -2,6 +2,18 @@
 {
     static void Main(string[] args)
     {
-       
+        Random rdn = new Random();
+        int random = rdn.Next(1, 11);
+
+        int user;
+        bool valid = false;
+        int i = 0;
+        bool[] array = new bool[10];
+        Console.WriteLine();
+
+        Console.Title = "Guess the number!";
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Guess the number from 1 to 10!\n ");
+        Console.ResetColor();
     }
 }
